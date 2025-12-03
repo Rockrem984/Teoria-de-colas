6f5be4fcb488ccaa554e99822a668e34a7dbf32d
Carlos Manuel <d24390246@huauchinango.tecnm.mx>
2025-12-03 03:43:41 +0000

    Avance

--- Teoria_de_colas.java
+++ Teoria_de_colas.java
@@ -14,15 +14,30 @@ public class Teoria_de_colas{
 
             switch(opcion){
                 case 1:
-                    System.out.println("Ingrese la tasa de llegadas: ");
+                    System.out.println("Ingrese la tasa de llegadas (λ): ");
                     double lambda = lector.nextDouble();
-                    System.out.println("Ingrese la tasa del servicio: ");
+                    System.out.println("Ingrese la tasa del servicio (μ): ");
                     double miu = lector.nextDouble();
 
                     double p = lambda/miu;
                     double Lq = Math.pow(lambda, 2)/(miu*(miu-lambda));
                     double Wq = Lq/lambda;
                     double W = (Wq+(1/miu));
+
+                    System.out.println("Resulatdos");
+                    System.out.println("Utilizacion de sistema (P): "+ p);
+                    System.out.println("Numero promedio de clientes en el sistema (Lq): "+Lq);
+                    System.out.println("Tiempo de promedio de espera en la cola (Wq): "+Wq);
+                    System.out.println("Tiempo total en el sistema (W): "+W);
+                    break;
+
+                    case 2:
+
+                    break;
+
+                    case 3:
+
+                    break;
             }
 
         }while (opcion!=4);
