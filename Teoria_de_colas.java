50e00205a4978a4554d7ec0c385e8d9b18add43b
Carlos Manuel <d24390246@huauchinango.tecnm.mx>
2025-12-03 03:14:50 +0000

    Se agrego el menu

--- Teoria_de_colas.java
+++ Teoria_de_colas.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
-public class Colas{
+public class Teoria_de_colas{
     public static void main(String[] args){
         Scanner lector = new Scanner(System.in);
+        byte opcion = 0;
+        do{
+            System.out.println("\n \t Menu");
+            System.out.println("1. Modelo M/M/1 ");
+            System.out.println("2. Modelo M/M/2 ");
+            System.out.println("3. Modelo M/M/3 ");
+            System.out.println("4. Salir ");
+            System.out.println("Opcion ");
+            opcion = lector.nextByte();
+
+        }while (opcion!=4);
 
     }
 }
