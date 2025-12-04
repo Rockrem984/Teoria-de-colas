23616c94e9e92a1d697d5a0bca2f8e76dafef31e
Carlos Manuel <d24390246@huauchinango.tecnm.mx>
2025-12-04 13:31:39 +0000

    Se corrigio el nombre del modelo

--- Teoria_de_colas.java
+++ Teoria_de_colas.java
@@ -7,7 +7,7 @@ public class Teoria_de_colas{
             System.out.println("\n \t Menu");
             System.out.println("1. Modelo M/M/1 ");
             System.out.println("2. Modelo M/M/1/K ");
-            System.out.println("3. Modelo M/M/3 ");
+            System.out.println("3. Modelo M/M/c ");
             System.out.println("4. Salir ");
             System.out.println("Opcion ");
             opcion = lector.nextByte();
